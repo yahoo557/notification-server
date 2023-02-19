ee1d1cc3d837b780dccf0312e562745adb191f75
baeklee <77774331+monjk557@users.noreply.github.com>
2023-02-19 23:54:34 +0900

    feat: 에러 처리중 ;; 왜뜨노

--- src/notifiations/notifications.module.ts
+++ src/notifiations/notifications.module.ts
@@ -1,28 +1,16 @@
 import { Module } from '@nestjs/common';
-import {ClientsModule, Transport} from "@nestjs/microservices";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 import {notificationsController} from "./notifications.controller";
+
+
 
 @Module({
     imports:[
-        ClientsModule.register([
-        {
-            name: 'NOTIFICATION_MICROSERVICE',
-            transport: Transport.KAFKA,
-            options:{
-                client:{
-                    clientId: 'notification',
-                    brokers:['localhost:9092'],
-                },
-                consumer:{
-                    groupId:'notification-consumer'
-                }
-            }
-        }]
-    )],
-    providers: [],
-    controllers: [notificationsController],
-})
 
+    ],
+    providers: [],
+    controllers: [],
+})
 export class NotificationsModule {}
 
 
