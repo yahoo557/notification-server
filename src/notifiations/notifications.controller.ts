3caa4530d1208be9c52baada530d7c0f3bb64ca8
baeklee <77774331+monjk557@users.noreply.github.com>
2023-02-20 23:22:44 +0900

    feat: kafka client 생성

--- src/notifiations/notifications.controller.ts
+++ src/notifiations/notifications.controller.ts
@@ -1,18 +1,19 @@
 import {Controller} from "@nestjs/common";
-import {EventPattern, Payload} from "@nestjs/microservices";
-
-interface notificationEvent {
-    token:String
-    sender:String
-}
+import {Client, ClientKafka, EventPattern, Payload, Transport} from "@nestjs/microservices";
 
 @Controller()
-export class notificationsController {
-    constructor(private readonly ) {}
-
-    @EventPattern('like_notification_created')
-    async handleNotificationCreate(@Payload()data: notificationEvent) {
-        console.log(data);
-    }
-
+export class HeroesController {
+    @Client({
+        transport: Transport.KAFKA,
+        options: {
+            client: {
+                clientId: 'notification', // hero-client
+                brokers: ['localhost:9092'],
+            },
+            consumer: {
+                groupId: 'hero-consumer' // hero-consumer-client
+            }
+        }
+    })
+    client: ClientKafka;
 }
