63620ac5ce1433903be1486cc1fb1b5b80a7bd04
baeklee <77774331+monjk557@users.noreply.github.com>
2023-03-02 22:43:49 +0900

    feat: 기능추가

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import {NestFactory} from '@nestjs/core';
-import {ClientsModule, MicroserviceOptions, Transport} from "@nestjs/microservices";
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
 import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
       AppModule,
       {
-        transport: Transport.KAFKA,
-          options:{
-            client:{
-                brokers:['3.36.212.116:9092'],
-            },
+          transport: Transport.KAFKA,
+          options: {
+              client:{
+                  clientId:'notifications',
+                  brokers:['15.165.177.220:9092'],
+              },
+              consumer: {
+                  groupId: 'notifications-consumer-1',
+              },
           }
+
       });
     await app.listen()
     console.log('Kafka consumer is listening')
