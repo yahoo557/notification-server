c3b8b6a6c6091f8a9e6b33918ebf41c826a11d48
baeklee <77774331+monjk557@users.noreply.github.com>
2023-03-02 22:43:08 +0900

    feat: 서비스추가 및 메시지 파싱 기능 추가중

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,19 @@
 import {Controller} from "@nestjs/common";
 import {EventPattern, MessagePattern, Payload} from "@nestjs/microservices";
-
+import {NotificationsService} from "./notifications/notifications.service"
+import {NotificationsModule} from "./notifications/notifications.module";
 @Controller()
-export class notificationsController {
-    @MessagePattern('notification-events')
+export class AppController {
+
+
+    //kafka message 는 message:String/deviceToken:String/date:DateTime 으로 되어있음
+    @MessagePattern('notification')
     getMessage(@Payload() message){
-        console.log(message.value);
+        const notificationService = new NotificationsService()
+
+        const parsedMessage = message.split("/");
+
+
+
     }
 }
